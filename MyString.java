f7a5c7c38fd3769e0f29b118bfe143f09539a5bc
grace-mao23 <gmao00@stuy.edu>
2018-11-21 14:29:44 -0500

    Wrote constructor with unfilled in methods

--- MyString.java
+++ MyString.java
@@ -2,7 +2,13 @@
 
 public class MyString implements CharSequence{
   private char[] data;
+
+  // Creates array from CharSequence given
   public MyString(CharSequence s){
+    char[] data = new char[s.length()];
+    for (int i = 0; i < s.length(); i++) {
+      data[i] = s.charAt(i);
+    }
   }
 
   public char charAt(int index) {
